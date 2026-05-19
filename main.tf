5c6279e3fe3154152dfd4fa3f5f3c6e7bd9fb6fc
Suheib Marzouka <suheib.marzouka@outlook.de>
2026-05-19 11:34:03 +0200

    lock state testen

--- main.tf
+++ main.tf
@@ -7,4 +7,7 @@ resource "aws_instance" "erste_instanzen" {
   ami             = "ami-051eaec1417c5d4ae"
   security_groups = ["default"]
   key_name = "25-06-key"
+  tags = {
+    Name = "1. Push"
+  }
 }
